be8c304dcd112025079194c26b81ce0eff60ca0f
José Víctor Ardasse <josevictorps3lima@gmail.com>
2024-03-29 19:40:50 -0300

    feat: login page improvment with Toastr and style

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,14 +2,20 @@ import { Component, Input } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponent,
+  DefaultLoginLayoutComponent,
     ReactiveFormsModule,
     PrimaryInputComponent
+  ],
+  providers: [
+    LoginService
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -17,11 +23,25 @@ import { PrimaryInputComponent } from '../../components/primary-input/primary-in
 export class LoginComponent {
   loginForm!: FormGroup;
 
-
-  constructor() { 
+  constructor(
+    private router: Router,
+    private LoginService: LoginService,
+    private toastService: ToastrService
+  ) { 
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     })
+  }
+
+  submit() {
+    this.LoginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success('Login efetuado com sucesso!'),
+      error: () => this.toastService.error('Erro no backend'),
+    })
+  }
+
+  navigate() {
+    this.router.navigate(['signup'])
   }
 }
